9d1c1dd6510092f2ba9b0c6cd8467f4b627a6fbf
TrentonTabor <ttabor@nrec.ri.cmu.edu>
2023-12-22 17:13:21 -0500

    simple matlab script for viewing grasps on a mesh

---
+++ adv/adv-grasp/visGraspScript.m
@@ -0,0 +1,21 @@
+bar = readObj('data/bar_clamp.obj')
+%%
+figure;
+hold on;
+trisurf(bar.f.v,bar.v(:,1),bar.v(:,2),bar.v(:,3))
+alpha 0.5
+for i = 1:size(bar.f.v,1)
+    triangle = bar.v(bar.f.v(i,:)',:);
+    triangleCenter = mean(triangle,1);
+    text(triangleCenter(1),triangleCenter(2),triangleCenter(3),num2str(i));
+    if i == 252
+        plot3(triangle(:,1),triangle(:,2),triangle(:,3),'r*')
+    end
+end
+%pcshow(bar.v(:,1:3))
+ray_o = [[-0.0472,  0.0167, -0.0634];
+        [ 0.0028,  0.0167, -0.0634]];
+ray_d = [[ 1.,  0.,  0.]
+        [-1., -0., -0.]] * 0.05/2;
+quiver3(ray_o(:,1),ray_o(:,2),ray_o(:,3),...
+    ray_d(:,1),ray_d(:,2),ray_d(:,3),'r','LineWidth',2,'AutoScale','off' ,'MaxHeadSize',1)
