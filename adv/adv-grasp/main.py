1a7b47814c92ae173e2faa5318ef22d4a30baf96
Hailie Mitchell <mitchelh@dickinson.edu>
2024-02-01 22:21:19 -0500

    a few interesting results from main.py

--- adv/adv-grasp/main.py
+++ adv/adv-grasp/main.py
@@ -5,13 +5,13 @@ from run_gqcnn import *
 # sample grasps
 renderer = Renderer()
 mesh, _ = renderer.render_object("data/bar_clamp.obj", display=False)
-grasps = Grasp.sample_grasps("data/new_barclamp.obj", 1, renderer=renderer)
+grasps = Grasp.sample_grasps("data/new_barclamp.obj", 5, renderer=renderer)
 
 # initialize model and attacks
 model = KitModel("weights.npy")
 model.eval()
-run1 = Attack(num_plots=10, steps_per_plot=50, model=model, renderer=renderer, oracle_method="dexnet")
-run2 = Attack(num_plots=10, steps_per_plot=50, model=model, renderer=renderer, oracle_method="pytorch")
+run1 = Attack(num_plots=10, steps_per_plot=10, model=model, renderer=renderer, oracle_method="dexnet")
+# run2 = Attack(num_plots=10, steps_per_plot=10, model=model, renderer=renderer, oracle_method="pytorch")
 
 # run attacks on each grasp
 print("num grasps:", len(grasps))
